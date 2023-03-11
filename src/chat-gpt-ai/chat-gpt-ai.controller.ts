e0a0371e8174a8c8c2c9245f4a019c5d4419d3bc
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2023-03-11 16:28:02 -0100

    added class validators

--- src/chat-gpt-ai/chat-gpt-ai.controller.ts
+++ src/chat-gpt-ai/chat-gpt-ai.controller.ts
@@ -1,12 +1,14 @@
+import { SetSelectedModel } from './model/set-selected-model';
 import { ModelAnswer } from './model/model.answer';
 import { ChatGptAiService } from './chat-gpt-ai.service';
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 
 @Controller('chat-gpt-ai')
 export class ChatGptAiController {
   constructor(private readonly chatGptAiService: ChatGptAiService) {}
 
   @Post('/message')
+  @UsePipes(ValidationPipe)
   getModelAnswer(@Body() data: ModelAnswer) {
     return this.chatGptAiService.getModelAnswer(data.question);
   }
@@ -14,5 +16,11 @@ export class ChatGptAiController {
   @Get()
   listModels() {
     return this.chatGptAiService.listModels();
+  }
+
+  @Post('/model')
+  @UsePipes(ValidationPipe)
+  setModel(@Body() data: SetSelectedModel){
+    this.chatGptAiService.setModelId(data.modelId);
   }
 }
--- src/chat-gpt-ai/model/model.answer.ts
+++ src/chat-gpt-ai/model/model.answer.ts
@@ -1,4 +1,16 @@
+import { IsString ,IsNotEmpty, IsNumber, IsOptional } from "class-validator"
+
 export class ModelAnswer{
 
+    @IsNumber()
+    @IsOptional()
+    temperature: number
+
+    @IsString()
+    @IsOptional()
+    modelId: string
+
+    @IsString()
+    @IsNotEmpty()
     question: string
 }
